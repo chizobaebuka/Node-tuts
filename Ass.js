f971c59d7fa9000f7d11af31b7593c6d77be366e
Decagon <decagon@Decagons-MacBook-Air.local>
2023-06-07 11:11:14 +0100

    manipulating url using http module

--- Ass.js
+++ Ass.js
@@ -1,39 +1,36 @@
-const http = require('http');
-const fs = require('fs');
+import https from 'https';
 
-const url = 'http://dhq.sh/ages.json';
-const outputFilePath = 'sum.txt';
+const url = 'https://dhq.sh/ages.json';
 
-http.get(url, (res) => {
+const request = https.get(url, (res) => {
   let data = '';
-
   res.on('data', (chunk) => {
     data += chunk;
-  });
+  })
 
   res.on('end', () => {
-    try {
-      const jsonData = JSON.parse(data);
-      const ages = jsonData.ages;
-      let sum = 0;
-
-      // Calculate the sum of ages greater than 50 using a for loop
-      for (let i = 0; i < ages.length; i++) {
-        if (ages[i] > 50) {
-          sum += ages[i];
-        }
-      }
-
-      // Write the sum to a file
-      const writeStream = fs.createWriteStream(outputFilePath);
-      writeStream.write(`Sum of ages greater than 50: ${sum}`);
-      writeStream.end();
-
-      console.log(`Sum of ages greater than 50 has been written to ${outputFilePath}`);
-    } catch (error) {
-      console.error('Error parsing JSON:', error.message);
+    try{
+      const json = JSON.parse(data);
+      calculateAges(json.data)
+      // console.log(json.data)
+    }catch(err){
+      console.error(err, `Error fetching Data`)
     }
-  });
-}).on('error', (error) => {
-  console.error('Error retrieving data:', error.message);
-});
+  })
+})
+
+request.on('err', (err)=>{
+  console.log(err)
+})
+
+function calculateAges(data){
+  const asParams = data.replaceAll(', ', '&');
+  const params = new URLSearchParams(asParams);
+  const ages = params.getAll('age');
+
+  const result = ages.map((age) => Number(age));
+  const filter = result.filter((age) => age > 50);
+
+  const sum = filter.reduce((total, age) => total + age, 0);
+  // filter
+}
